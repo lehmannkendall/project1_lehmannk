98f4e41b3e1a9c985203cbc95c671dcde84f168a
Kendall Lehmann <lehmannk@umich.edu>
2019-02-04 21:45:23 -0500

    first commit

--- lab3_code.py
+++ lab3_code.py
@@ -99,31 +99,6 @@ class Pound(Currency):
             c_name = "Pounds"
         return "{} {}".format(self.value, c_name)
 
-## The code provided below, when un-commented, should work when your class definitions are complete. Of course, you should also submit to the autograder to see your test results.
-
-### PROVIDED CODE you can try:
-# dollar = Dollar(1)
-# pound = Pound(1)
-# yuan = Yuan(1)
-#
-# print(yuan.conversion(Pound))
-# #0.5333333333333333 Pound
-#
-# print(pound.conversion(Pound))
-# #1.0 Pound
-#
-# print(pound.conversion(Dollar))
-# #0.75 Dollar
-#
-# print(dollar)
-# #1 Dollar
-#
-# two_dollars = Dollar(2)
-# print(two_dollars)
-# #2 Dollars
-
-
-
 ## [STEP 2]
 # Define code for a class Bank, as follows.
 
@@ -166,24 +141,40 @@ class Bank:
             # or self.current_account.value += 1
             return "successful deposit"
 
+if __name__ == "__main__":
+    dollar = Dollar(1)
+    pound = Pound(1)
+    yuan = Yuan(1)
 
-## The below code provided, when un-commented, should work when your class definitions are complete. Of course, you should also submit to the autograder to see your test results.
+    print(yuan.conversion(Pound))
+    #0.5333333333333333 Pound
 
-#### PROVIDED CODE:
-## Creating instances
-# jpMorgan = Bank("J.P.Morgan", Dollar, 1)
-# barclays = Bank("Barclays", Pound, 1)
-# bank_of_china = Bank("Bank of China", Yuan, 1)
-#
-#
-# print(jpMorgan.current_account.value)
-# #1
-#
-# print(jpMorgan.deposit(dollar))
-# # should show: 'successful deposit'
-#
-# print(jpMorgan.current_account.value)
-# # should show: 2
-#
-# print(jpMorgan.deposit(pound))
-# # should show: 'ERROR: cannot deposit that currency.'
+    print(pound.conversion(Pound))
+    #1.0 Pound
+
+    print(pound.conversion(Dollar))
+    #0.75 Dollar
+
+    print(dollar)
+    #1 Dollar
+
+    two_dollars = Dollar(2)
+    print(two_dollars)
+    #2 Dollars
+
+    # Creating instances
+    jpMorgan = Bank("J.P.Morgan", Dollar, 1)
+    barclays = Bank("Barclays", Pound, 1)
+    bank_of_china = Bank("Bank of China", Yuan, 1)
+
+    print(jpMorgan.current_account.value)
+    #1
+
+    print(jpMorgan.deposit(dollar))
+    # should show: 'successful deposit'
+
+    print(jpMorgan.current_account.value)
+    # should show: 2
+
+    print(jpMorgan.deposit(pound))
+    # should show: 'ERROR: cannot deposit that currency.'
